68dedf8190e16fa114a6417e199aadea326f173e
mmmihhaeel <mikael11032005@gmail.com>
2023-12-28 21:02:11 +0300

    initialized array and added printArray function

--- main.cpp
+++ main.cpp
@@ -2,10 +2,21 @@
 
 using namespace std;
 
-int main()
-{
-    
-    cout << "Hello world" << endl;
+// Функція для виведення масиву на екран
+void printArray(const int* array, int size) {
+    for (int i = 0; i < size; i++) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    const int size = 14;
+    int array[size] = {2, -5, 7, -10, 15, -3, 8, 4, -1, 9, 12, 6, 18, 20};
+
+    cout << "Initialized array: ";
+    printArray(array, size);
+
 
     return 0;
 }
